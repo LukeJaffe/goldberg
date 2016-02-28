b12e8281869e1a82cce3091cca2ee255e1a14d37
Luke Jaffe <lukejaffe1@gmail.com>
2016-02-28 14:55:01 -0500

    Restructure color vertices

--- python/vert.py
+++ python/vert.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 C = []
 V = []
 F = []
@@ -32,9 +34,11 @@ for line in f:
             n = n.strip(",")
             F[-1].append(V[int(n)])
 
+frames = deepcopy(F)
+faces = deepcopy(F)
+
 # Reorder face vertices to draw with triangle strip
-"""
-for f in F:
+for f in faces:
     # Hexagon
     if len(f) == 6:
         f[3], f[5] = f[5], f[3]
@@ -43,7 +47,7 @@ for f in F:
     elif len(f) == 5:
         f[3], f[4] = f[4], f[3]
         f[2], f[3] = f[3], f[2]
-"""
+
 
 def write_face_vertices(out, f):
     out.write("vertexBuffers.push(gl.createBuffer());\n")
@@ -57,22 +61,47 @@ def write_face_vertices(out, f):
     out.write("vertexBuffers[vertexBuffers.length - 1].itemSize = 3;\n");
     out.write("vertexBuffers[vertexBuffers.length - 1].numItems = "+str(len(f))+";\n\n");
 
-def write_face_colors(out, f, c):
+def write_face_colors(out, n, c):
     out.write("colorBuffers.push(gl.createBuffer());\n")
     out.write("gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffers[colorBuffers.length - 1]);\n")
     out.write("var colors =\n") 
     out.write("[\n")
-    for v in f:  
+    for i in range(n):  
         out.write("%f, %f, %f, %f,\n" % c)
     out.write("];\n")
     out.write("gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);\n")
     out.write("colorBuffers[colorBuffers.length - 1].itemSize = 4;\n");
-    out.write("colorBuffers[colorBuffers.length - 1].numItems = 3;\n\n");
+    out.write("colorBuffers[colorBuffers.length - 1].numItems = %d;\n\n" % n);
 
 # Create the javascript initBuffers() function, write it to a file
 out = open("verts.js", "wb")
-out.write("function initBuffers(vertexBuffers, colorBuffers)\n{\n")
-for f in F:
+
+# Write vertex buffers
+out.write("function initFrameBuffers(vertexBuffers)\n{\n")
+for f in frames:
     write_face_vertices(out, f)
-    write_face_colors(out, f, (0.0, 1.0, 0.0, 1.0))
+out.write("}\n");
+
+out.write("function initFaceBuffers(vertexBuffers)\n{\n")
+for f in faces:
+    write_face_vertices(out, f)
+out.write("}\n");
+
+# Write color buffers
+colors = [
+(1.0, 0.0, 0.0, 1.0),
+(0.0, 1.0, 0.0, 1.0),
+(0.0, 0.0, 1.0, 1.0),
+(1.0, 1.0, 1.0, 1.0),
+(0.0, 0.0, 0.0, 1.0)
+]
+
+out.write("function initColorBuffers5(colorBuffers)\n{\n")
+for c in colors:
+    write_face_colors(out, 5, c)
+out.write("}\n");
+
+out.write("function initColorBuffers6(colorBuffers)\n{\n")
+for c in colors:
+    write_face_colors(out, 6, c)
 out.write("}\n");
